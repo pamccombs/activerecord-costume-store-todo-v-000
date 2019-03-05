c994869ca2e7213e48f543b3cc3248fd5e725f14
Patrick McCombs <pa.mccombs1@gmail.com>
2019-03-05 11:26:04 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,7 +2,9 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.string :breed
+      t.string :price
+      t.string :size
+      t.string :image
     end
   end
 end
