90a54f70cefb3736bf2786bec705b7e5bbfe590f
Patrick McCombs <pa.mccombs1@gmail.com>
2019-03-05 11:28:38 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,12 +3,12 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
-      t.float :price
       t.string :theme
-      t.boolean :family_friendly
+      t.float :price
+      t.boolean :family_friendly?
       t.datetime :opening_date
       t.datetime :closing_date
-      t.text :description
+      t.text :long_description
       
       t.timestamps null: false
     end
